0280c70c59a672f2538223c45558f6e99ac3325d
Corri <95197153+corricodes@users.noreply.github.com>
2023-02-05 20:37:03 -0500

    completed components section

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,17 @@
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 
   return (
-    <div className='expense-item'>
-      <div className='expense-item__date'>{props.date.toISOString(0)}</div>
+    <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
       </div>
       <div className='expense-item__price'>${props.amount}</div>
-    </div>
+    </Card>
   );
 }
 
