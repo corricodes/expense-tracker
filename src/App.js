cd7e6fc4089765a4a86f4e6ed552c1559e3df592
Corri <95197153+corricodes@users.noreply.github.com>
2023-02-06 13:16:34 -0500

    began building expense form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -28,6 +29,7 @@ const App = () => {
   return (
     <div>
       <h2 className='expense-title'>Expenses</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
